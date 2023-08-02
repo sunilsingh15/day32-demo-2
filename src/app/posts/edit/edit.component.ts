cfc2f45b380ff22ef71aecdd8b497c6533db2671
sunilsingh15 <sunilsingh.1996@hotmail.com>
2023-08-03 00:34:36 +0800

    demo completed

--- src/app/posts/edit/edit.component.ts
+++ src/app/posts/edit/edit.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PostService } from '../post.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Post } from '../post';
 
 @Component({
   selector: 'app-edit',
@@ -11,13 +12,40 @@ import { Router } from '@angular/router';
 export class EditComponent implements OnInit {
   editForm: FormGroup;
 
-  constructor(private postSvc: PostService, private router: Router, private fb: FormBuilder) { }
-
-  ngOnInit(): void {
-    
+  postObject: Post = {
+    id: 0,
+    title: '',
+    body: ''
   }
 
-  editPost() {
+  constructor(private postSvc: PostService, private router: Router, private route: ActivatedRoute, private fb: FormBuilder) { }
 
+  ngOnInit(): void {
+    this.editForm = this.fb.group({
+      title: [this.postObject.title, Validators.required],
+      body: [this.postObject.body, Validators.required]
+    });
+
+    this.route.paramMap.subscribe((param) => {
+      var id = Number(param.get('postID'));
+      this.getPostByID(id);
+    })
+  }
+
+  getPostByID(id: number) {
+    this.postSvc.getByID(id).subscribe((data) => {
+      this.postObject = data;
+    })
+  }
+
+  update() {
+    this.postSvc.update(this.postObject.id, this.postObject).subscribe({
+      next: (data) => {
+        this.router.navigate(['/posts/home']);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
   }
 }
--- src/app/posts/post-details/post-details.component.ts
+++ src/app/posts/post-details/post-details.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../post';
+import { PostService } from '../post.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-post-details',
   templateUrl: './post-details.component.html',
   styleUrls: ['./post-details.component.css']
 })
-export class PostDetailsComponent {
+export class PostDetailsComponent implements OnInit {
+
+  postObject: Post = {
+    id: 0,
+    title: '',
+    body: ''
+  }
+
+  constructor(private postSvc: PostService, private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((param) => {
+      var id = Number(param.get('postID'));
+
+      this.postSvc.getByID(id).subscribe((data) => {
+        this.postObject = data;
+      })
+    })
+  }
 
 }
